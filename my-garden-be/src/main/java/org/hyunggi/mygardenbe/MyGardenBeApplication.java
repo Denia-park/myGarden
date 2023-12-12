d2ddf683f1af579fa11b06e7cd3d20a407bddddb
HyunggiPark <phg9587@naver.com>
2023-12-12 20:48:25 +0900

    [be] git init

---
+++ my-garden-be/src/main/java/org/hyunggi/mygardenbe/MyGardenBeApplication.java
@@ -0,0 +1,13 @@
+package org.hyunggi.mygardenbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGardenBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyGardenBeApplication.class, args);
+    }
+
+}
---
+++ my-garden-be/src/test/java/org/hyunggi/mygardenbe/MyGardenBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hyunggi.mygardenbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGardenBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
