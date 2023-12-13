aeda46ee83ebb34aceda620a075a2ead2c9fcef0
HyunggiPark <phg9587@naver.com>
2023-12-13 23:56:17 +0900

    [fe] feat: 기본으로 사용할 Header 및 Footer 추가

--- my-garden-fe/src/App.vue
+++ my-garden-fe/src/App.vue
@@ -1,8 +1,12 @@
 <script setup>
 import DailyRoutine from "@/DailyRoutine.vue";
+import DefaultHeader from "@/DefaultHeader.vue";
+import DefaultFooter from "@/DefaultFooter.vue";
 </script>
 
 <template>
+  <DefaultHeader/>
   <DailyRoutine/>
+  <DefaultFooter/>
 </template>
 
