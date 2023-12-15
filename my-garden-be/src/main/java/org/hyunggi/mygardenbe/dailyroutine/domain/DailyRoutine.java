049e529df36143d1d5a7ef11fbdefdfac00d38ff
HyunggiPark <phg9587@naver.com>
2023-12-15 20:47:58 +0900

    [be] feat: validate 추가

--- my-garden-be/src/main/java/org/hyunggi/mygardenbe/dailyroutine/domain/DailyRoutine.java
+++ my-garden-be/src/main/java/org/hyunggi/mygardenbe/dailyroutine/domain/DailyRoutine.java
@@ -1,6 +1,7 @@
 package org.hyunggi.mygardenbe.dailyroutine.domain;
 
 import lombok.Getter;
+import org.springframework.util.Assert;
 
 @Getter
 public class DailyRoutine {
@@ -9,8 +10,16 @@ public class DailyRoutine {
     private String routineDescription;
 
     public DailyRoutine(RoutineTime routineTime, String routineType, String routineDescription) {
+        validateConstructor(routineTime, routineType, routineDescription);
+
         this.routineTime = routineTime;
         this.routineType = RoutineType.valueOf(routineType);
         this.routineDescription = routineDescription;
+    }
+
+    private void validateConstructor(final RoutineTime routineTime, final String routineType, final String routineDescription) {
+        Assert.isTrue(routineTime != null, "데일리 루틴의 시간은 null이 될 수 없습니다.");
+        Assert.hasText(routineType, "데일리 루틴의 타입은 비어있을 수 없습니다.");
+        Assert.isTrue(routineDescription != null, "데일리 루틴의 설명은 null이 될 수 없습니다.");
     }
 }
