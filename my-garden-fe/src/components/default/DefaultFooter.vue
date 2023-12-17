aabfddb90c5a3c18e01106d33531748b65f7d02e
HyunggiPark <phg9587@naver.com>
2023-12-18 02:16:07 +0900

    [fe] chore: Footer 사이즈 및 문구 수정

--- my-garden-fe/src/components/default/DefaultFooter.vue
+++ my-garden-fe/src/components/default/DefaultFooter.vue
@@ -3,14 +3,14 @@
 </script>
 
 <template>
-  <footer class="text-muted py-5">
+  <footer class="text-muted py-3">
     <div class="container">
       <p class="float-end mb-1">
         <a href="#">Back to top</a>
       </p>
-      <p class="mb-1">Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
-      <p class="mb-0">New to Bootstrap? <a href="/my-garden-fe/public">Visit the homepage</a> or read our <a
-          href="/docs/5.0/getting-started/introduction/">getting started guide</a>.</p>
+      <p class="mb-1">myGarden 서비스에 많은 관심 부탁드립니다.
+        [ <a href="https://github.com/Denia-park/myGarden">Project Github Link</a> ]
+      </p>
     </div>
   </footer>
 </template>
