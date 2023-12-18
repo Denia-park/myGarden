f02929c47837359b7756bfcdb035430557e7cdac
HyunggiPark <phg9587@naver.com>
2023-12-19 01:39:44 +0900

    [be] feat: DailyRoutine 등록시에 설명 텍스트는 255자를 넘지 못하도록 validation 추가

--- my-garden-be/src/main/java/org/hyunggi/mygardenbe/dailyroutine/controller/request/PostRequest.java
+++ my-garden-be/src/main/java/org/hyunggi/mygardenbe/dailyroutine/controller/request/PostRequest.java
@@ -2,6 +2,7 @@ package org.hyunggi.mygardenbe.dailyroutine.controller.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
@@ -13,6 +14,7 @@ public record PostRequest(
         @NotBlank(message = "데일리 루틴의 타입은 비어있을 수 없습니다.")
         String routineType,
         @NotNull(message = "데일리 루틴의 설명은 null이 될 수 없습니다.")
+        @Size(max = 255, message = "데일리 루틴의 설명은 255자를 넘을 수 없습니다.")
         String routineDescription
 ) {
 }
