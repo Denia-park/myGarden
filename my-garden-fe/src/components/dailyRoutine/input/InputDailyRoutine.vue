a1f66d5ebb1b8e49d4aae53e3df0b0733eb51340
HyunggiPark <phg9587@naver.com>
2023-12-19 00:34:48 +0900

    [fe] chore: validate 메서드 추가 및 등록시에 서버로 데이터 전송

--- my-garden-fe/src/components/dailyRoutine/input/InputDailyRoutine.vue
+++ my-garden-fe/src/components/dailyRoutine/input/InputDailyRoutine.vue
@@ -3,27 +3,75 @@ import ContentTitle from "@/components/default/ContentTitle.vue";
 import DateInput from "@/components/dailyRoutine/input/DateInput.vue";
 import ContentInput from "@/components/dailyRoutine/input/ContentInput.vue";
 import {ref} from "vue";
+import axios from "axios";
 
 const startDate = ref('');
 const endDate = ref('');
+const content = ref('');
 
-function addLog(content) {
-
+function postDailyRoutine() {
   console.log("startDate : " + startDate.value)
   console.log("endDate : " + endDate.value)
   console.log("content : " + content.value)
 
-  // if (todo.value.trim() && startDate.value && endDate.value) {
-  //   logs.value.push({
-  //     id: Date.now(),
-  //     text: todo.value,
-  //     start: startDate.value,
-  //     end: endDate.value
-  //   });
-  //   todo.value = '';
-  //   saveLogs();
-  // }
+  axios.post('/api/daily-routine', {
+        startDateTime: startDate.value,
+        endDateTime: endDate.value,
+        routineDescription: content.value
+      }
+  ).then(() => {
+        alert("등록되었습니다.");
+        location.reload();
+      }
+  ).catch(error => {
+        console.log(error);
+      }
+  );
 }
+
+function addLog(inputContent) {
+  content.value = inputContent.value;
+
+  validate();
+  postDailyRoutine();
+}
+
+function validate() {
+  //시작 날짜가 끝 날짜보다 늦을 수 없다.
+  validateStartDateIsBeforeEndDate();
+
+  //두 날짜는 1일이상 차이가 날 수 없다.
+  validateDateDifferenceIsSmaller1();
+
+  //content는 255자를 넘을 수 없다.
+  validateContentLength();
+}
+
+function validateStartDateIsBeforeEndDate() {
+  if (startDate.value > endDate.value) {
+    alert("시작 날짜가 끝 날짜보다 늦을 수 없습니다.")
+  }
+}
+
+function validateDateDifferenceIsSmaller1() {
+  // Calculate the time difference in milliseconds
+  const timeDifference = new Date(endDate.value) - new Date(startDate.value);
+
+  // Calculate the number of days difference
+  const daysDifference = timeDifference / (1000 * 3600 * 24);
+
+  // Check if the difference is more than 2 days
+  if (daysDifference >= 1) {
+    alert("1일 이상 차이 날 수 없습니다.");
+  }
+}
+
+function validateContentLength() {
+  if (content.value.length > 255) {
+    alert("content는 255자를 넘을 수 없습니다.")
+  }
+}
+
 </script>
 
 <template>
