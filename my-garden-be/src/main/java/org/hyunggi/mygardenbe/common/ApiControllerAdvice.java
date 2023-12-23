12f0eea814d953eb077f0bb93b203830abae7e0b
HyunggiPark <phg9587@naver.com>
2023-12-23 23:55:21 +0900

    [be] chore: 예외 발생시에 Log를 남기도록 수정

--- my-garden-be/src/main/java/org/hyunggi/mygardenbe/common/ApiControllerAdvice.java
+++ my-garden-be/src/main/java/org/hyunggi/mygardenbe/common/ApiControllerAdvice.java
@@ -1,5 +1,6 @@
 package org.hyunggi.mygardenbe.common;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,14 +8,18 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class ApiControllerAdvice {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(BindException.class)
     public ApiResponse<Object> bindException(BindException e) {
+        final String errorMessage = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+        log.warn("API Controller BindException : {}", errorMessage);
+
         return ApiResponse.of(
                 HttpStatus.BAD_REQUEST,
-                e.getBindingResult().getAllErrors().get(0).getDefaultMessage(),
+                errorMessage,
                 null
         );
     }
