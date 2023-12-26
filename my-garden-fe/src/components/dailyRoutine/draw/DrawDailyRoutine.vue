f51392104a5b360ec6cab0b1c113f0a75d606cea
HyunggiPark <phg9587@naver.com>
2023-12-26 19:43:53 +0900

    [fe] feat: TimeBlock에 Tooltip 적용

--- my-garden-fe/src/components/dailyRoutine/draw/DrawDailyRoutine.vue
+++ my-garden-fe/src/components/dailyRoutine/draw/DrawDailyRoutine.vue
@@ -123,8 +123,9 @@ const afternoonSchedule = computed(() => {
       <div class="schedule-half">
         <div v-for="(block, index) in morningSchedule" :key="`morning-${index}`"
              :style="blockStyle(block, 'morning')"
-             class="time-block">
+             class="time-block my-tooltip">
           <div>{{ block.displayStartTime }} ~ {{ block.displayEndTime }} :: {{ block.routineType }}</div>
+          <span class="tooltiptext tooltip-left">왼쪽 툴팁</span>
         </div>
       </div>
     </div>
@@ -134,8 +135,9 @@ const afternoonSchedule = computed(() => {
       <div class="schedule-half">
         <div v-for="(block, index) in afternoonSchedule" :key="`afternoon-${index}`"
              :style="blockStyle(block, 'afternoon')"
-             class="time-block">
+             class="time-block my-tooltip">
           <div>{{ block.displayStartTime }} ~ {{ block.displayEndTime }} :: {{ block.routineType }}</div>
+          <span class="tooltiptext tooltip-right">오른쪽 툴팁</span>
         </div>
       </div>
     </div>
@@ -179,5 +181,60 @@ const afternoonSchedule = computed(() => {
   font-size: 1.2rem;
   text-align: center;
   box-sizing: border-box;
+}
+
+.my-tooltip {
+  position: relative;
+  display: block;
+}
+
+.my-tooltip .tooltiptext {
+  visibility: hidden; /* 이벤트가 없으면 툴팁 영역을 숨김 */
+  width: 120px; /* 툴팁 영역의 넓이를 설정 */
+  background-color: black;
+  color: #fff;
+  text-align: center;
+  border-radius: 6px;
+  padding: 5px 0;
+
+  position: absolute; /* 절대 위치를 사용 */
+  z-index: 1;
+}
+
+.my-tooltip:hover .tooltiptext {
+  visibility: visible; /* hover 이벤트 발생시 영역을 보여줌 */
+}
+
+.my-tooltip .tooltiptext::after {
+  content: " "; /* 정사각형 영역 사용 */
+  position: absolute; /* 절대 위치 사용 */
+  border-style: solid;
+  border-width: 5px; /* 테두리 넓이를 5px 로 설정 */
+}
+
+.my-tooltip .tooltip-left {
+  top: -5px; /* 영역의 위치를 -5 만큼 위로 이동 */
+  right: 105%; /* 왼쪽에 생성해야하므로 영역을 오른쪽에서 105% 이동 */
+}
+
+.my-tooltip .tooltip-left::after {
+  top: 50%; /* 사각형 영역이 중앙에 오도록 위치 */
+  left: 100%; /* 왼쪽에서 100% 위치에 오도록 위치 */
+  margin-top: -5px;
+
+  /* 사각형의 테두리에서 왼쪽만 노출 */
+  border-color: transparent transparent transparent black;
+}
+
+.my-tooltip .tooltip-right {
+  top: -5px;
+  left: 105%;
+}
+
+.my-tooltip .tooltip-right::after {
+  top: 50%;
+  right: 100%;
+  margin-top: -5px;
+  border-color: transparent black transparent transparent;
 }
 </style>
