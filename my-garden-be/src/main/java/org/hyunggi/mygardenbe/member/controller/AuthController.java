a1eda5832490a90be2de861b8e45e8f720685081
HyunggiPark <phg9587@naver.com>
2024-01-06 01:59:18 +0900

    [be] refact: 이름 변경

--- my-garden-be/src/main/java/org/hyunggi/mygardenbe/member/controller/AuthController.java
+++ my-garden-be/src/main/java/org/hyunggi/mygardenbe/member/controller/AuthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class MemberController {
+public class AuthController {
     private final MemberService memberService;
 
 }
