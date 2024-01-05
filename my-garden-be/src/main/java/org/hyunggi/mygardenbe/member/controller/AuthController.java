c69f039ced96c44a4684fa980179b91de1ebaa38
HyunggiPark <phg9587@naver.com>
2024-01-06 02:21:50 +0900

    [be] feat: 간이 로그인 기능 추가

--- my-garden-be/src/main/java/org/hyunggi/mygardenbe/member/controller/AuthController.java
+++ my-garden-be/src/main/java/org/hyunggi/mygardenbe/member/controller/AuthController.java
@@ -1,12 +1,36 @@
 package org.hyunggi.mygardenbe.member.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.hyunggi.mygardenbe.common.response.ApiResponse;
+import org.hyunggi.mygardenbe.member.entity.MemberEntity;
+import org.hyunggi.mygardenbe.member.repository.MemberRepository;
 import org.hyunggi.mygardenbe.member.service.MemberService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
     private final MemberService memberService;
 
+    private final MemberRepository memberRepository;
+
+
+    @PostMapping("/api/auth/sign-up")
+    public void signUp() {
+
+    }
+
+    @PostMapping("/api/auth/login")
+    public ApiResponse<Long> signIn(@RequestBody Map<String, String> paramas) {
+        final MemberEntity member = memberRepository.findByEmailAndPassword(
+                paramas.get("email"),
+                paramas.get("password")
+        ).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
+
+        return ApiResponse.ok(member.getId());
+    }
 }
--- my-garden-be/src/main/java/org/hyunggi/mygardenbe/member/repository/MemberRepository.java
+++ my-garden-be/src/main/java/org/hyunggi/mygardenbe/member/repository/MemberRepository.java
@@ -3,6 +3,8 @@ package org.hyunggi.mygardenbe.member.repository;
 import org.hyunggi.mygardenbe.member.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
-    MemberEntity findByEmailAndPassword(String email, String password);
+    Optional<MemberEntity> findByEmailAndPassword(String email, String password);
 }
