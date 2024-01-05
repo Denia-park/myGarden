76d8df57c4c22d6ecd687cdaa62a79e3e4bcfed9
HyunggiPark <phg9587@naver.com>
2024-01-05 22:34:40 +0900

    [fe] feat: Vue router 추가 및 간이 Login Page 추가

--- my-garden-fe/src/App.vue
+++ my-garden-fe/src/App.vue
@@ -1,12 +1,11 @@
 <script setup>
-import DailyRoutine from "@/components/dailyRoutine/DailyRoutine.vue";
 import DefaultHeader from "@/components/default/DefaultHeader.vue";
 import DefaultFooter from "@/components/default/DefaultFooter.vue";
 </script>
 
 <template>
   <DefaultHeader/>
-  <DailyRoutine/>
+  <RouterView/>
   <DefaultFooter/>
 </template>
 
---
+++ my-garden-fe/src/components/login/Login.vue
@@ -0,0 +1,52 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="form-sign-in w-100 m-auto">
+    <form>
+      <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+      <div class="form-floating">
+        <input id="floatingInput" class="form-control" placeholder="name@example.com" type="email">
+        <label for="floatingInput">Email address</label>
+      </div>
+      <div class="form-floating">
+        <input id="floatingPassword" class="form-control" placeholder="Password" type="password">
+        <label for="floatingPassword">Password</label>
+      </div>
+
+      <div class="form-check text-start my-3">
+        <input id="flexCheckDefault" class="form-check-input" type="checkbox" value="remember-me">
+        <label class="form-check-label" for="flexCheckDefault">
+          Remember me
+        </label>
+      </div>
+      <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+      <p class="mt-5 mb-3 text-body-secondary">&copy; 2017–2023</p>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+.form-sign-in {
+  max-width: 330px;
+  padding: 1rem;
+}
+
+.form-sign-in .form-floating:focus-within {
+  z-index: 2;
+}
+
+.form-sign-in input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+
+.form-sign-in input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+</style>
