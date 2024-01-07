d68321a29903b9150c369f11ef747db6eaab91da
HyunggiPark <phg9587@naver.com>
2024-01-08 02:16:20 +0900

    [fe] refact: router 추출

--- my-garden-fe/src/main.js
+++ my-garden-fe/src/main.js
@@ -2,19 +2,7 @@ import './assets/main.css'
 
 import {createApp} from 'vue'
 import App from './App.vue'
-import {createRouter, createWebHistory} from 'vue-router'
-import DailyRoutine from "@/components/dailyRoutine/DailyRoutine.vue";
-import Login from "@/components/login/Login.vue";
 import {store} from "@/scripts/store.js";
-
-const routes = [
-    {path: '/', component: DailyRoutine},
-    {path: '/login', component: Login},
-]
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
+import {router} from "@/scripts/router.js";
 
 createApp(App).use(store).use(router).mount('#app')
