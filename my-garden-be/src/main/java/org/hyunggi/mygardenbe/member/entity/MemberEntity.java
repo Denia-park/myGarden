f62bf3ce7b65a09175b25e9e5101705243c21118
HyunggiPark <phg9587@naver.com>
2024-01-10 02:16:29 +0900

    [be] feat: MemberEntity가 userDetails를 구현

--- my-garden-be/src/main/java/org/hyunggi/mygardenbe/member/entity/MemberEntity.java
+++ my-garden-be/src/main/java/org/hyunggi/mygardenbe/member/entity/MemberEntity.java
@@ -1,18 +1,20 @@
 package org.hyunggi.mygardenbe.member.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hyunggi.mygardenbe.common.entity.BaseEntity;
 import org.hyunggi.mygardenbe.member.domain.Member;
+import org.hyunggi.mygardenbe.member.domain.Role;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Getter
 @NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 @Entity
-public class MemberEntity extends BaseEntity {
+public class MemberEntity extends BaseEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     private Long id;
@@ -20,6 +22,12 @@ public class MemberEntity extends BaseEntity {
     private String email;
     @Column(length = 100, nullable = false)
     private String password;
+    @Getter
+    @Enumerated(EnumType.STRING)
+    @Column(length = 10, nullable = false)
+    private Role role;
+
+    private boolean enabled;
 
     private MemberEntity(final String email, final String password) {
         this.email = email;
@@ -38,5 +46,40 @@ public class MemberEntity extends BaseEntity {
                 email,
                 password
         );
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.role.getAuthorities();
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.enabled;
     }
 }
