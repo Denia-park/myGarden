a3d98b252288d0a373e5a452099874d9e430f1f3
HyunggiPark <phg9587@naver.com>
2024-01-11 01:01:09 +0900

    [be] refact: AuthController 패키지 수정

--- my-garden-be/src/main/java/org/hyunggi/mygardenbe/auth/controller/AuthController.java
+++ my-garden-be/src/main/java/org/hyunggi/mygardenbe/auth/controller/AuthController.java
@@ -1,18 +1,22 @@
-package org.hyunggi.mygardenbe.member.controller;
+package org.hyunggi.mygardenbe.auth.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.hyunggi.mygardenbe.member.repository.MemberRepository;
 import org.hyunggi.mygardenbe.member.service.MemberService;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping(AuthController.AUTH_API_PATH)
 public class AuthController {
+    public static final String AUTH_API_PATH = "/api/auth";
+
     private final MemberService memberService;
     private final MemberRepository memberRepository;
 
-    @PostMapping("/api/auth/sign-up")
+    @PostMapping("/sign-up")
     public void signUp() {
 
     }
