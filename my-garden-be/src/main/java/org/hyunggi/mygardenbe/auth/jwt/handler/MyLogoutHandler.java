cd670234bfef7b2116111843a8e853e5cc4a7c0e
HyunggiPark <phg9587@naver.com>
2024-01-11 01:10:56 +0900

    [be] feat: MyLogoutHandler 추가

---
+++ my-garden-be/src/main/java/org/hyunggi/mygardenbe/auth/jwt/handler/MyLogoutHandler.java
@@ -0,0 +1,36 @@
+package org.hyunggi.mygardenbe.auth.jwt.handler;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.hyunggi.mygardenbe.auth.jwt.domain.TokenType;
+import org.hyunggi.mygardenbe.auth.jwt.entity.TokenEntity;
+import org.hyunggi.mygardenbe.auth.jwt.repository.TokenRepository;
+import org.hyunggi.mygardenbe.auth.jwt.util.JwtAuthUtil;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class MyLogoutHandler implements LogoutHandler {
+
+    private final TokenRepository tokenRepository;
+
+    @Override
+    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        String accessTokenText = JwtAuthUtil.extractTokenTextFromRequestHeader(request, TokenType.BEARER);
+
+        if (accessTokenText == null) {
+            return;
+        }
+
+        final TokenEntity storedToken = tokenRepository.findByTokenText(accessTokenText).orElse(null);
+        if (storedToken != null) {
+            storedToken.revoke();
+            tokenRepository.save(storedToken);
+            SecurityContextHolder.clearContext();
+        }
+    }
+}
