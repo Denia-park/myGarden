8321db42bf3e6a572a3d94e55b6db913ab075b08
HyunggiPark <phg9587@naver.com>
2024-01-11 02:33:34 +0900

    [be] refact: 사용하지 않는 메서드 삭제
    -

--- my-garden-be/src/main/java/org/hyunggi/mygardenbe/member/repository/MemberRepository.java
+++ my-garden-be/src/main/java/org/hyunggi/mygardenbe/member/repository/MemberRepository.java
@@ -6,7 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
-    Optional<MemberEntity> findByEmailAndPassword(final String email, final String password);
-
     Optional<MemberEntity> findByEmail(final String email);
 }
