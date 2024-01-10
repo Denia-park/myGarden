2ca216bb35c30cf4e1b1ba2d32bda72d1a015aeb
HyunggiPark <phg9587@naver.com>
2024-01-11 01:35:36 +0900

    [be] feat: Security 및 Jwt 설정에 필요한 파일 추가
    -

--- my-garden-be/src/main/java/org/hyunggi/mygardenbe/auth/jwt/repository/TokenRepository.java
+++ my-garden-be/src/main/java/org/hyunggi/mygardenbe/auth/jwt/repository/TokenRepository.java
@@ -2,6 +2,7 @@ package org.hyunggi.mygardenbe.auth.jwt.repository;
 
 import org.hyunggi.mygardenbe.auth.jwt.entity.TokenEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -10,4 +11,7 @@ public interface TokenRepository extends JpaRepository<TokenEntity, Long> {
     Optional<TokenEntity> findByMemberId(final Long memberId);
 
     Optional<TokenEntity> findByTokenText(final String tokenText);
+
+    @Query("select t from TokenEntity t join MemberEntity m on t.memberId = m.id where m.email = :userEmail")
+    Optional<TokenEntity> findTokenByUserEmail(String userEmail);
 }
