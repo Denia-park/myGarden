c29882d3d012a1a014677321b700a26fc6064d59
HyunggiPark <phg9587@naver.com>
2024-01-12 22:06:21 +0900

    [be] feat: ControllerTest에서 Security 기능도 테스트가 가능 하도록 설정
    - JwtAuthenticationFilter는 필요없으므로 주입 받지 않음
    - 내가 설정한 SecurityConfiguration을 쓰기 위해 MyCustomTestSecurityConfiguration 추가
    - 로그인 된 유저가 접근한다고 가정하기 위해 @WithMyCustomUser 추가

--- my-garden-be/src/test/java/org/hyunggi/mygardenbe/ControllerTestSupport.java
+++ my-garden-be/src/test/java/org/hyunggi/mygardenbe/ControllerTestSupport.java
@@ -1,16 +1,33 @@
 package org.hyunggi.mygardenbe;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hyunggi.mygardenbe.auth.jwt.filter.JwtAuthenticationFilter;
+import org.hyunggi.mygardenbe.auth.jwt.service.MyLogoutHandler;
 import org.hyunggi.mygardenbe.dailyroutine.controller.DailyRoutineController;
 import org.hyunggi.mygardenbe.dailyroutine.service.DailyRoutineService;
+import org.hyunggi.mygardenbe.mock.security.MyCustomTestSecurityConfiguration;
+import org.hyunggi.mygardenbe.mock.security.WithMyCustomUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(controllers = {
-        DailyRoutineController.class,
-})
+@WebMvcTest(
+        controllers = {
+                DailyRoutineController.class,
+        },
+
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = JwtAuthenticationFilter.class)
+        },
+
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = MyCustomTestSecurityConfiguration.class)
+        }
+)
+@WithMyCustomUser
 public abstract class ControllerTestSupport {
     @Autowired
     protected MockMvc mockMvc;
@@ -20,4 +37,7 @@ public abstract class ControllerTestSupport {
 
     @MockBean
     protected DailyRoutineService dailyRoutineService;
+
+    @MockBean
+    protected MyLogoutHandler myLogoutHandler;
 }
