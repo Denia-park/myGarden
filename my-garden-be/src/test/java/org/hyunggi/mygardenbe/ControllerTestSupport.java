801e9e3738b146dd27d5c844c3358b28c8af1bd4
HyunggiPark <phg9587@naver.com>
2024-01-13 00:01:41 +0900

    [be] refact: ControllerTestSupport에서 MockUser가 포함된 클래스를 따로 생성

---
+++ my-garden-be/src/test/java/org/hyunggi/mygardenbe/ControllerTestSupport.java
@@ -0,0 +1,47 @@
+package org.hyunggi.mygardenbe;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hyunggi.mygardenbe.auth.controller.AuthController;
+import org.hyunggi.mygardenbe.auth.jwt.filter.JwtAuthenticationFilter;
+import org.hyunggi.mygardenbe.auth.jwt.service.MyLogoutHandler;
+import org.hyunggi.mygardenbe.dailyroutine.controller.DailyRoutineController;
+import org.hyunggi.mygardenbe.dailyroutine.service.DailyRoutineService;
+import org.hyunggi.mygardenbe.member.service.MemberService;
+import org.hyunggi.mygardenbe.mock.security.MyCustomTestSecurityConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(
+        controllers = {
+                AuthController.class,
+                DailyRoutineController.class
+        },
+
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = JwtAuthenticationFilter.class)
+        },
+
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = MyCustomTestSecurityConfiguration.class)
+        }
+)
+public abstract class ControllerTestSupport {
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @MockBean
+    protected MyLogoutHandler myLogoutHandler;
+
+    @MockBean
+    protected DailyRoutineService dailyRoutineService;
+
+    @MockBean
+    protected MemberService memberService;
+}
--- my-garden-be/src/test/java/org/hyunggi/mygardenbe/ControllerTestSupportWithMockUser.java
+++ my-garden-be/src/test/java/org/hyunggi/mygardenbe/ControllerTestSupportWithMockUser.java
@@ -1,43 +1,9 @@
 package org.hyunggi.mygardenbe;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.hyunggi.mygardenbe.auth.jwt.filter.JwtAuthenticationFilter;
-import org.hyunggi.mygardenbe.auth.jwt.service.MyLogoutHandler;
-import org.hyunggi.mygardenbe.dailyroutine.controller.DailyRoutineController;
-import org.hyunggi.mygardenbe.dailyroutine.service.DailyRoutineService;
-import org.hyunggi.mygardenbe.mock.security.MyCustomTestSecurityConfiguration;
 import org.hyunggi.mygardenbe.mock.security.WithMyCustomUser;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(
-        controllers = {
-                DailyRoutineController.class,
-        },
 
-        excludeFilters = {
-                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = JwtAuthenticationFilter.class)
-        },
-
-        includeFilters = {
-                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = MyCustomTestSecurityConfiguration.class)
-        }
-)
 @WithMyCustomUser
-public abstract class ControllerTestSupportWithMockUser {
-    @Autowired
-    protected MockMvc mockMvc;
+public abstract class ControllerTestSupportWithMockUser extends ControllerTestSupport {
 
-    @Autowired
-    protected ObjectMapper objectMapper;
-
-    @MockBean
-    protected DailyRoutineService dailyRoutineService;
-
-    @MockBean
-    protected MyLogoutHandler myLogoutHandler;
 }
