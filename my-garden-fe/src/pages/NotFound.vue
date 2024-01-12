292b1728af65d1a9ecf1573b2be1e3f2649476bb
HyunggiPark <phg9587@naver.com>
2024-01-13 03:24:10 +0900

    [fe] feat: NotFound 페이지 추가

---
+++ my-garden-fe/src/pages/NotFound.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h1>해당 페이지를 찾을 수 없습니다.</h1>
+    <p>요청 주소를 다시 한번 확인해주세요.</p>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 3rem;
+  font-weight: bold;
+  margin-bottom: 2rem;
+  text-align: center;
+}
+
+p {
+  font-size: 1.5rem;
+  text-align: center;
+}
+</style>
