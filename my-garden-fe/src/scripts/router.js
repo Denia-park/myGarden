be976abe5b033867cf77f8307f96b06828a7c02d
HyunggiPark <phg9587@naver.com>
2024-01-15 23:19:07 +0900

    [fe] feat: root경로 접근시 로그인 페이지로 이동

--- my-garden-fe/src/scripts/router.js
+++ my-garden-fe/src/scripts/router.js
@@ -4,10 +4,11 @@ import {createRouter, createWebHistory} from "vue-router";
 import NotFound from "@/pages/NotFound.vue";
 
 const routes = [
-    {path: '/', component: DailyRoutine},
     {path: '/login', component: Login},
     {path: '/notFound', component: NotFound},
-    {path: "/:pathMatch(.*)*", redirect: "/notFound"}
+
+    {path: '/', redirect: '/login'},
+    {path: '/:pathMatch(.*)*', redirect: '/notFound'}
 ]
 
 export const router = createRouter({
