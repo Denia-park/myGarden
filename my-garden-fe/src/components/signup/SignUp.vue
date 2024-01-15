f072a4cdbe0a7932f94098afa45579df4c619337
HyunggiPark <phg9587@naver.com>
2024-01-16 00:28:34 +0900

    [fe] chore: 소셜로그인은 추후에 기능 추가

--- my-garden-fe/src/components/signup/SignUp.vue
+++ my-garden-fe/src/components/signup/SignUp.vue
@@ -86,27 +86,27 @@ function validatePassword() {
           </div>
           <button class="w-100 mb-2 btn btn-lg rounded-3 btn-primary" type="submit" @click="signup">Sign up</button>
           <small class="text-body-secondary">By clicking Sign up, you agree to the terms of use.</small>
-          <hr class="my-4">
-          <h2 class="fs-5 fw-bold mb-3">Or use a third-party</h2>
-          <button class="w-100 py-2 mb-2 btn btn-outline-warning rounded-3" type="submit">
-            <svg class="bi me-1" height="16" width="16">
-              <use xlink:href="#kakao"/>
-              <i class="fa-regular fa-comment"></i>
-            </svg>
-            Sign up with Kakao
-          </button>
-          <button class="w-100 py-2 mb-2 btn btn-outline-success rounded-3" type="submit">
-            <svg class="bi me-1" height="16" width="16">
-              <i class="fa-regular fa-n"></i>
-            </svg>
-            Sign up with Naver
-          </button>
-          <button class="w-100 py-2 mb-2 btn btn-outline-primary rounded-3" type="submit">
-            <svg class="bi me-1" height="16" width="16">
-              <i class="fa-brands fa-google"></i>
-            </svg>
-            Sign up with Google
-          </button>
+          <!--          <hr class="my-4">-->
+          <!--          <h2 class="fs-5 fw-bold mb-3">Or use a third-party</h2>-->
+          <!--          <button class="w-100 py-2 mb-2 btn btn-outline-warning rounded-3" type="submit">-->
+          <!--            <svg class="bi me-1" height="16" width="16">-->
+          <!--              <use xlink:href="#kakao"/>-->
+          <!--              <i class="fa-regular fa-comment"></i>-->
+          <!--            </svg>-->
+          <!--            Sign up with Kakao-->
+          <!--          </button>-->
+          <!--          <button class="w-100 py-2 mb-2 btn btn-outline-success rounded-3" type="submit">-->
+          <!--            <svg class="bi me-1" height="16" width="16">-->
+          <!--              <i class="fa-regular fa-n"></i>-->
+          <!--            </svg>-->
+          <!--            Sign up with Naver-->
+          <!--          </button>-->
+          <!--          <button class="w-100 py-2 mb-2 btn btn-outline-primary rounded-3" type="submit">-->
+          <!--            <svg class="bi me-1" height="16" width="16">-->
+          <!--              <i class="fa-brands fa-google"></i>-->
+          <!--            </svg>-->
+          <!--            Sign up with Google-->
+          <!--          </button>-->
         </div>
       </div>
     </div>
