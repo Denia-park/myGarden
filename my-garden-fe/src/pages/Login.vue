ae68d9e8c18915b54ca20812ed1865354af9277c
HyunggiPark <phg9587@naver.com>
2024-01-16 00:31:41 +0900

    [fe] chore: Remember me는 추후에 기능 추가

--- my-garden-fe/src/pages/Login.vue
+++ my-garden-fe/src/pages/Login.vue
@@ -51,10 +51,10 @@ function goToSignup() {
           </div>
 
           <div class="form-check text-start my-3">
-            <input id="flexCheckDefault" class="form-check-input" type="checkbox" value="remember-me">
-            <label class="form-check-label" for="flexCheckDefault">
-              Remember me
-            </label>
+            <!--            <input id="flexCheckDefault" class="form-check-input" type="checkbox" value="remember-me">-->
+            <!--            <label class="form-check-label" for="flexCheckDefault">-->
+            <!--              Remember me-->
+            <!--            </label>-->
           </div>
           <button class="btn btn-primary w-100 py-2" @click="submit">Login</button>
           <button class="btn btn-success w-100 py-2" @click="goToSignup">Sign up</button>
