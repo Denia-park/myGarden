33b4aaf8f828687b7e124f49c85768689fe18c89
HyunggiPark <phg9587@naver.com>
2024-01-16 22:52:22 +0900

    [fe] feat: 서버 새로고침시에 SessionStorage에서 Token 값을 읽어서 vuex에 적용

--- my-garden-fe/src/App.vue
+++ my-garden-fe/src/App.vue
@@ -1,6 +1,9 @@
 <script setup>
 import DefaultHeader from "@/components/default/DefaultHeader.vue";
 import DefaultFooter from "@/components/default/DefaultFooter.vue";
+import {store} from "@/scripts/store.js";
+
+store.dispatch('initializeTokenFromSessionStorage');
 </script>
 
 <template>
