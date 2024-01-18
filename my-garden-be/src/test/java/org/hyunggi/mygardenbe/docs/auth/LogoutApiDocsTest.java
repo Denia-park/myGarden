ab2c5c76a62f4134b72c491cb7a7c2d5f96b6c94
HyunggiPark <phg9587@naver.com>
2024-01-19 02:27:36 +0900

    [be] docs: LogoutApiDocsTest에 필요한 Header 내용 추가

--- my-garden-be/src/test/java/org/hyunggi/mygardenbe/docs/auth/LogoutApiDocsTest.java
+++ my-garden-be/src/test/java/org/hyunggi/mygardenbe/docs/auth/LogoutApiDocsTest.java
@@ -1,15 +1,16 @@
 package org.hyunggi.mygardenbe.docs.auth;
 
 import org.hyunggi.mygardenbe.ControllerTestSupport;
-import org.hyunggi.mygardenbe.auth.controller.request.RefreshRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
 
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
-import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -19,22 +20,19 @@ class LogoutApiDocsTest extends ControllerTestSupport {
     @DisplayName("로그아웃을 한다.")
     void logout() throws Exception {
         // given
-        final RefreshRequest request = new RefreshRequest("refreshToken");
+        final String refreshToken = "refreshToken";
 
         // when, then
         mockMvc.perform(
                         post("/api/auth/logout")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(objectMapper.writeValueAsString(request))
+                                .header("Authorization", "Bearer " + refreshToken)
                 )
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.data").doesNotExist())
                 .andDo(document("auth/logout"
                         , preprocessRequest(prettyPrint())
                         , preprocessResponse(prettyPrint())
-                        , requestFields(
-                                fieldWithPath("refreshToken").optional().description("로그아웃할 리프레시 토큰")
-                        )
+                        , requestHeaders(headerWithName("Authorization").description("로그아웃할 리프레시 토큰 ['Bearer {refreshToken}']"))
                         , responseFields(
                                 fieldWithPath("code").type(JsonFieldType.NUMBER).description("HTTP 상태 코드"),
                                 fieldWithPath("status").type(JsonFieldType.STRING).description("HTTP 상태"),
