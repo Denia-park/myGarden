50708bdbc2ff51e427e7d03c90732b9e6d524ac7
HyunggiPark <phg9587@naver.com>
2024-01-19 22:40:00 +0900

    [be] feat: AuthenticationPrincipal를 쉽게 받아올 수 있도록 Custom 어노테이션 추가

---
+++ my-garden-be/src/main/java/org/hyunggi/mygardenbe/common/auth/LoginUserEntity.java
@@ -0,0 +1,12 @@
+package org.hyunggi.mygardenbe.common.auth;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@AuthenticationPrincipal
+public @interface LoginUserEntity {
+}
