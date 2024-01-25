2cd6e8b4035b93003c8e1facc1c5921d7c43b43d
HyunggiPark <phg9587@naver.com>
2024-01-26 02:07:10 +0900

    [be] feat: NoticeBoardEntity 생성

---
+++ my-garden-be/src/main/java/org/hyunggi/mygardenbe/boards/notice/entity/NoticeBoardEntity.java
@@ -0,0 +1,121 @@
+package org.hyunggi.mygardenbe.boards.notice.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hyunggi.mygardenbe.common.entity.BaseEntity;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+@Entity
+public class NoticeBoardEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 100)
+    private String title;
+    @Column(nullable = false, length = 2000)
+    private String content;
+    @Column(nullable = false, length = 20)
+    private String category;
+    @Column(nullable = false, length = 10)
+    private Boolean isImportant;
+    @Column(nullable = false)
+    private Integer views;
+    @Column(nullable = false, length = 30)
+    private String writer;
+    @Column(nullable = false, length = 15)
+    private LocalDateTime writtenAt;
+    @Column(nullable = false)
+    private Long memberId;
+
+    @Builder(access = lombok.AccessLevel.PRIVATE)
+    private NoticeBoardEntity(final String title, final String content, final String category, final Boolean isImportant, final String writer, final LocalDateTime writtenAt, final Long memberId) {
+        this.title = title;
+        this.content = content;
+        this.category = category;
+        this.isImportant = isImportant;
+        this.writer = writer;
+        this.writtenAt = writtenAt;
+        this.memberId = memberId;
+
+        this.views = 0;
+    }
+
+    public static NoticeBoardEntity of(final String title, final String content, final String category, final String writer, final Long memberId) {
+        return of(title, content, category, false, writer, LocalDateTime.now(), memberId);
+    }
+
+    public static NoticeBoardEntity of(final String title, final String content, final String category, final Boolean isImportant, final String writer, final LocalDateTime writtenAt, final Long memberId) {
+        validateConstructor(title, content, category, isImportant, writer, writtenAt, memberId);
+
+        return NoticeBoardEntity.builder()
+                .title(title)
+                .content(content)
+                .category(category)
+                .isImportant(isImportant)
+                .writer(writer)
+                .writtenAt(writtenAt)
+                .memberId(memberId)
+                .build();
+    }
+
+    private static void validateConstructor(final String title, final String content, final String category, final Boolean isImportant, final String writer, final LocalDateTime writtenAt, final Long memberId) {
+        validateTitle(title);
+        validateContent(content);
+        validateCategory(category);
+        validateIsImportant(isImportant);
+        validateWriter(writer);
+        validateWrittenAt(writtenAt);
+        validateMemberId(memberId);
+    }
+
+    private static void validateTitle(final String title) {
+        if (title == null || title.isBlank()) {
+            throw new IllegalArgumentException("제목은 null이 될 수 없고 빈 문자열이 될 수 없습니다.");
+        }
+    }
+
+    private static void validateContent(final String content) {
+        if (content == null || content.isBlank()) {
+            throw new IllegalArgumentException("내용은 null이 될 수 없고 빈 문자열이 될 수 없습니다.");
+        }
+    }
+
+    private static void validateCategory(final String category) {
+        if (category == null || category.isBlank()) {
+            throw new IllegalArgumentException("카테고리는 null이 될 수 없고 빈 문자열이 될 수 없습니다.");
+        }
+    }
+
+    private static void validateIsImportant(final Boolean isImportant) {
+        if (isImportant == null) {
+            throw new IllegalArgumentException("알림글 여부는 null이 될 수 없습니다.");
+        }
+    }
+
+    private static void validateWriter(final String writer) {
+        if (writer == null || writer.isBlank()) {
+            throw new IllegalArgumentException("작성자는 null이 될 수 없고 빈 문자열이 될 수 없습니다.");
+        }
+    }
+
+    private static void validateWrittenAt(final LocalDateTime writtenAt) {
+        if (writtenAt == null) {
+            throw new IllegalArgumentException("작성일은 null이 될 수 없습니다.");
+        }
+    }
+
+    private static void validateMemberId(final Long memberId) {
+        if (memberId == null || memberId <= 0) {
+            throw new IllegalArgumentException("멤버 아이디는 null이 될 수 없고 0보다 커야 합니다.");
+        }
+    }
+
+    public void setImportant() {
+        this.isImportant = true;
+    }
+}
---
+++ my-garden-be/src/test/java/org/hyunggi/mygardenbe/boards/notice/entity/NoticeBoardEntityTest.java
@@ -0,0 +1,184 @@
+package org.hyunggi.mygardenbe.boards.notice.entity;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class NoticeBoardEntityTest {
+
+    @Test
+    @DisplayName("of() 메서드를 통해 NoticeEntity 객체를 생성할 수 있다.")
+    void of() {
+        // given
+        final String title = "title";
+        final String content = "content";
+        final String category = "category";
+        final Boolean isImportant = false;
+        final String writer = "writer";
+        final LocalDateTime writtenAt = LocalDateTime.now();
+        final Long memberId = 1L;
+
+        // when
+        NoticeBoardEntity noticeBoardEntity = NoticeBoardEntity.of(title, content, category, isImportant, writer, writtenAt, memberId);
+
+        // then
+        assertThat(noticeBoardEntity.getTitle()).isEqualTo(title);
+        assertThat(noticeBoardEntity.getContent()).isEqualTo(content);
+        assertThat(noticeBoardEntity.getCategory()).isEqualTo(category);
+        assertThat(noticeBoardEntity.getIsImportant()).isEqualTo(isImportant);
+        assertThat(noticeBoardEntity.getWriter()).isEqualTo(writer);
+        assertThat(noticeBoardEntity.getWrittenAt()).isEqualTo(writtenAt);
+        assertThat(noticeBoardEntity.getMemberId()).isEqualTo(memberId);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("title이 null 혹은 빈 문자열이면 IllegalArgumentException이 발생한다.")
+    void titleIsNull(final String title) {
+        // given
+        final String content = "content";
+        final String category = "category";
+        final Boolean isImportant = false;
+        final String writer = "writer";
+        final LocalDateTime writtenAt = LocalDateTime.now();
+        final Long memberId = 1L;
+
+        // when, then
+        assertThatThrownBy(() -> NoticeBoardEntity.of(title, content, category, isImportant, writer, writtenAt, memberId))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("제목은 null이 될 수 없고 빈 문자열이 될 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("content가 null 혹은 빈 문자열이면 IllegalArgumentException이 발생한다.")
+    void contentIsNull(final String content) {
+        // given
+        final String title = "title";
+        final String category = "category";
+        final Boolean isImportant = false;
+        final String writer = "writer";
+        final LocalDateTime writtenAt = LocalDateTime.now();
+        final Long memberId = 1L;
+
+        // when, then
+        assertThatThrownBy(() -> NoticeBoardEntity.of(title, content, category, isImportant, writer, writtenAt, memberId))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("내용은 null이 될 수 없고 빈 문자열이 될 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("category가 null 혹은 빈 문자열이면 IllegalArgumentException이 발생한다.")
+    void categoryIsNull(final String category) {
+        // given
+        final String title = "title";
+        final String content = "content";
+        final Boolean isImportant = false;
+        final String writer = "writer";
+        final LocalDateTime writtenAt = LocalDateTime.now();
+        final Long memberId = 1L;
+
+        // when, then
+        assertThatThrownBy(() -> NoticeBoardEntity.of(title, content, category, isImportant, writer, writtenAt, memberId))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("카테고리는 null이 될 수 없고 빈 문자열이 될 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("isImportant가 null이면 IllegalArgumentException이 발생한다.")
+    void isImportantIsNull() {
+        // given
+        final String title = "title";
+        final String content = "content";
+        final String category = "category";
+        final String writer = "writer";
+        final LocalDateTime writtenAt = LocalDateTime.now();
+        final Long memberId = 1L;
+
+        // when, then
+        assertThatThrownBy(() -> NoticeBoardEntity.of(title, content, category, null, writer, writtenAt, memberId))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("알림글 여부는 null이 될 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("writer가 null 혹은 빈 문자열이면 IllegalArgumentException이 발생한다.")
+    void writerIsNull(final String writer) {
+        // given
+        final String title = "title";
+        final String content = "content";
+        final String category = "category";
+        final Boolean isImportant = false;
+        final LocalDateTime writtenAt = LocalDateTime.now();
+        final Long memberId = 1L;
+
+        // when, then
+        assertThatThrownBy(() -> NoticeBoardEntity.of(title, content, category, isImportant, writer, writtenAt, memberId))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("작성자는 null이 될 수 없고 빈 문자열이 될 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("writtenAt이 null이면 IllegalArgumentException이 발생한다.")
+    void writtenAtIsNull() {
+        // given
+        final String title = "title";
+        final String content = "content";
+        final String category = "category";
+        final Boolean isImportant = false;
+        final String writer = "writer";
+        final Long memberId = 1L;
+
+        // when, then
+        assertThatThrownBy(() -> NoticeBoardEntity.of(title, content, category, isImportant, writer, null, memberId))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("작성일은 null이 될 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"0", "-1", ","})
+    @DisplayName("memberId가 null 혹은 0이하이면, IllegalArgumentException이 발생한다.")
+    void memberIdIsNull(final Long memberId) {
+        // given
+        final String title = "title";
+        final String content = "content";
+        final String category = "category";
+        final Boolean isImportant = false;
+        final String writer = "writer";
+        final LocalDateTime writtenAt = LocalDateTime.now();
+
+        // when, then
+        assertThatThrownBy(() -> NoticeBoardEntity.of(title, content, category, isImportant, writer, writtenAt, memberId))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("멤버 아이디는 null이 될 수 없고 0보다 커야 합니다.");
+    }
+
+    @Test
+    @DisplayName("setImportant() 메서드를 통해 isImportant를 true로 변경할 수 있다.")
+    void setImportant() {
+        // given
+        final String title = "title";
+        final String content = "content";
+        final String category = "category";
+        final Boolean isImportant = false;
+        final String writer = "writer";
+        final LocalDateTime writtenAt = LocalDateTime.now();
+        final Long memberId = 1L;
+        NoticeBoardEntity noticeBoardEntity = NoticeBoardEntity.of(title, content, category, isImportant, writer, writtenAt, memberId);
+
+        // when
+        noticeBoardEntity.setImportant();
+
+        // then
+        assertThat(noticeBoardEntity.getIsImportant()).isTrue();
+    }
+}
