343866b29ae906b517ee3bff1ce939f22d85502d
HyunggiPark <phg9587@naver.com>
2024-01-29 23:10:03 +0900

    [fe] feat: 페이지네이션 추가

--- my-garden-fe/src/components/boards/common/PaginationForm.vue
+++ my-garden-fe/src/components/boards/common/PaginationForm.vue
@@ -1,5 +1,14 @@
 <script setup>
 
+const props = defineProps({
+  pageInfo: {
+    type: Object,
+    required: true,
+  },
+});
+
+const emit = defineEmits(["page-change"]);
+
 </script>
 
 <template>
@@ -7,7 +16,7 @@
     <nav aria-label="Page navigation example">
       <ul class="pagination">
         <li class="page-item">
-          <a aria-label="Previous" class="page-link">
+          <a aria-label="Previous" class="page-link" @click="() => emit('page-change', 1)">
             <span aria-hidden="true">&laquo;</span>
           </a>
         </li>
@@ -16,10 +25,11 @@
             <span aria-hidden="true">&lt;</span>
           </a>
         </li>
-        <li class="page-item">
-          <a class="page-link"
-
-          ></a>
+        <li v-for="page in pageInfo.totalPages" class="page-item">
+          <a :id="page === pageInfo.currentPage ? 'currentPage' : ''" class="page-link"
+             @click="() => emit('page-change', page)">
+            {{ page }}
+          </a>
         </li>
         <li class="page-item">
           <a aria-label="Next" class="page-link">
@@ -27,7 +37,7 @@
           </a>
         </li>
         <li class="page-item">
-          <a aria-label="Next" class="page-link">
+          <a aria-label="Next" class="page-link" @click="() => emit('page-change', props.pageInfo.totalPages)">
             <span aria-hidden="true">&raquo;</span>
           </a>
         </li>
@@ -37,7 +47,6 @@
 </template>
 
 <style scoped>
-
 .align-center {
   display: flex;
   justify-content: center;
@@ -49,6 +58,12 @@
 
 .page-div a {
   color: black;
+  cursor: pointer;
+}
+
+#currentPage {
+  font-weight: bold;
+  color: red;
 }
 
 </style>
