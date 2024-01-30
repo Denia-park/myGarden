7d389d4c2a5a15502645e015e4e21d1577544b8c
HyunggiPark <phg9587@naver.com>
2024-01-31 02:27:14 +0900

    [be] feat: PostRequest 추가

---
+++ my-garden-be/src/main/java/org/hyunggi/mygardenbe/boards/learn/controller/request/PostRequest.java
@@ -0,0 +1,19 @@
+package org.hyunggi.mygardenbe.boards.learn.controller.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+
+@Builder
+public record PostRequest(
+        @NotBlank(message = "제목은 비어있을 수 없습니다.")
+        @Size(max = 100, message = "제목은 100자를 넘을 수 없습니다.")
+        String title,
+        @NotBlank(message = "분류는 비어있을 수 없습니다.")
+        @Size(max = 20, message = "분류는 20자를 넘을 수 없습니다.")
+        String category,
+        @NotBlank(message = "내용은 비어있을 수 없습니다.")
+        @Size(max = 4000, message = "내용은 4000자를 넘을 수 없습니다.")
+        String content
+) {
+}
