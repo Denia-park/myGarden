0e29c47037f1040567d2be250bb4c67b682ba6dc
HyunggiPark <phg9587@naver.com>
2024-02-05 00:14:38 +0900

    [be] chore: prod에서만 https 적용하도록 수정

--- my-garden-be/src/main/java/org/hyunggi/mygardenbe/configuration/ConnectorConfiguration.java
+++ my-garden-be/src/main/java/org/hyunggi/mygardenbe/configuration/ConnectorConfiguration.java
@@ -8,9 +8,11 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@Profile("prod")
 @Configuration
-public class ConnectorConfig {
+public class ConnectorConfiguration {
     @Bean
     public ServletWebServerFactory servletContainer() {
 
