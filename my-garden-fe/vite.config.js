b9bf0ccace48943d5a5abe9795992f8199c59d8b
HyunggiPark <phg9587@naver.com>
2024-02-05 00:16:01 +0900

    [fe] chore: Local에서 vue를 실행할 때, https 관련 에러가 발생하지 않도록 처리

--- my-garden-fe/vite.config.js
+++ my-garden-fe/vite.config.js
@@ -1,24 +1,30 @@
 import {fileURLToPath, URL} from 'node:url'
 
-import {defineConfig} from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [
-        vue(),
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
+export default ({mode}) => {
+    process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+    const {VITE_API_URL} = process.env;
+
+    return defineConfig({
+        plugins: [
+            vue(),
+        ],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url))
+            }
+        },
+        server: {
+            proxy: {
+                '/api': VITE_API_URL || 'https://localhost'
+            }
+        },
+        build: {
+            outDir: '../my-garden-be/src/main/resources/static'
         }
-    },
-    server: {
-        proxy: {
-            '/api': 'https://localhost'
-        }
-    },
-    build: {
-        outDir: '../my-garden-be/src/main/resources/static'
-    }
-})
+    });
+}
