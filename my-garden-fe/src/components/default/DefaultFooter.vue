fa3d8da2df3503c823745be6ff2173c84291bffa
HyunggiPark <phg9587@naver.com>
2024-02-07 21:45:16 +0900

    temp

--- my-garden-fe/src/components/default/DefaultFooter.vue
+++ my-garden-fe/src/components/default/DefaultFooter.vue
@@ -8,7 +8,7 @@
       <p class="float-end mb-1">
         <a href="#">Back to top</a>
       </p>
-      <p class="mb-1">myGarden 서비스에 많은 관심 부탁드립니다.</p>
+      <p class="mb-1">myGarden 서비스에 많은 관심 부탁드립니다. 감사합니다</p>
       <p class="mb-1"> [ <a href="https://github.com/Denia-park/myGarden" target="_blank">Project Github Link</a> ]
       </p>
     </div>
