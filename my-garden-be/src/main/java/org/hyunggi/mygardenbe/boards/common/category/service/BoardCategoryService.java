9285a38d4cf6bcb1648ae66ff273903e1c7c6f63
HyunggiPark <phg9587@naver.com>
2024-02-21 00:52:34 +0900

    [be] feat: BoardCategoryService의 메서드에 검증 로직 추가

--- my-garden-be/src/main/java/org/hyunggi/mygardenbe/boards/common/category/service/BoardCategoryService.java
+++ my-garden-be/src/main/java/org/hyunggi/mygardenbe/boards/common/category/service/BoardCategoryService.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.hyunggi.mygardenbe.boards.common.category.repository.BoardCategoryRepository;
 import org.hyunggi.mygardenbe.boards.common.category.response.BoardCategoryResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import java.util.List;
 
@@ -14,13 +15,25 @@ public class BoardCategoryService {
     private final BoardCategoryRepository boardCategoryRepository;
 
     public List<BoardCategoryResponse> getCategories(final String boardType) {
+        validateBoardType(boardType);
         return boardCategoryRepository.findAllByBoardType(boardType).stream()
                 .map(BoardCategoryResponse::of)
                 .toList();
     }
 
+    private void validateBoardType(final String boardType) {
+        Assert.hasText(boardType, "게시판 타입은 비어있을 수 없습니다.");
+    }
+
     public void validateCategoryWithBoardType(final String category, final String boardType) {
+        validateCategory(category);
+        validateBoardType(boardType);
+        
         boardCategoryRepository.findByCodeAndBoardType(category, boardType)
                 .orElseThrow(() -> new EntityNotFoundException("해당 분류가 존재하지 않습니다."));
+    }
+
+    private void validateCategory(final String category) {
+        Assert.hasText(category, "분류는 비어있을 수 없습니다.");
     }
 }
--- my-garden-be/src/test/java/org/hyunggi/mygardenbe/boards/common/category/service/BoardCategoryServiceTest.java
+++ my-garden-be/src/test/java/org/hyunggi/mygardenbe/boards/common/category/service/BoardCategoryServiceTest.java
@@ -7,6 +7,8 @@ import org.hyunggi.mygardenbe.boards.common.category.repository.BoardCategoryRep
 import org.hyunggi.mygardenbe.boards.common.category.response.BoardCategoryResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -38,6 +40,46 @@ class BoardCategoryServiceTest extends IntegrationTestSupport {
         assertThat(categories).hasSize(1)
                 .extracting("code", "text")
                 .containsExactly(tuple("project", "프로젝트"));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("분류 종류를 가져올 때, 해당 하는 게시판 타입이 존재하지 않으면 예외를 발생시킨다.")
+    void getCategoriesWithNullBoardType(final String boardType) {
+        //when, then
+        assertThatThrownBy(() -> boardCategoryService.getCategories(boardType))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("게시판 타입은 비어있을 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("분류를 검증할 때, 해당 하는 게시판 타입이 Null이거나 빈 문자열이면 예외를 발생시킨다.")
+    void validateBoardType(final String boardType) {
+        //given
+        BoardCategoryEntity boardCategoryEntity = new BoardCategoryEntity("project", "프로젝트", "notice");
+
+        boardCategoryRepository.save(boardCategoryEntity);
+
+        //when, then
+        assertThatThrownBy(() -> boardCategoryService.validateCategoryWithBoardType("study", boardType))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("게시판 타입은 비어있을 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("해당 하는 카테고리가 Null이거나 빈 문자열이면 예외를 발생시킨다.")
+    void validateCategory(final String category) {
+        //given
+        BoardCategoryEntity boardCategoryEntity = new BoardCategoryEntity("project", "프로젝트", "notice");
+
+        boardCategoryRepository.save(boardCategoryEntity);
+
+        //when, then
+        assertThatThrownBy(() -> boardCategoryService.validateCategoryWithBoardType(category, "notice"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("분류는 비어있을 수 없습니다.");
     }
 
     @Test
