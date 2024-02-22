0f9afb8b2b6cd5a5a5611d1dc3499b19cab7c69d
HyunggiPark <phg9587@naver.com>
2024-02-23 01:11:59 +0900

    [fe] chore: router에 하루 일과 통계 추가

--- my-garden-fe/src/scripts/router.js
+++ my-garden-fe/src/scripts/router.js
@@ -10,12 +10,14 @@ import NoticeBoardWrite from "@/pages/boards/notice/NoticeBoardWrite.vue";
 import LearnBoardList from "@/pages/boards/learn/LearnBoardList.vue";
 import LearnBoardView from "@/pages/boards/learn/LearnBoardView.vue";
 import LearnBoardWrite from "@/pages/boards/learn/LearnBoardWrite.vue";
+import Statistics from "@/pages/dailyRoutine/Statistics.vue";
 
 const routes = [
     {path: '/login', component: Login},
     {path: '/signup', component: SignUp},
     {path: '/notFound', component: NotFound},
     {path: '/daily-routine', component: DailyRoutine, meta: {permitRoles: ['ROLE_USER', 'ROLE_ADMIN']}},
+    {path: '/daily-routine/statistics', component: Statistics, meta: {permitRoles: ['ROLE_USER', 'ROLE_ADMIN']}},
     {path: '/boards/notice', component: NoticeBoardList, name: 'NoticeBoardList'},
     {path: '/boards/notice/:boardId', component: NoticeBoardView, name: 'NoticeBoardView'},
     {
