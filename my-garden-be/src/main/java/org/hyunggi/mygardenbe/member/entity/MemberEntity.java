54838c8609209a2df273703a74a880d21e643a8d
HyunggiPark <phg9587@naver.com>
2024-03-14 17:03:44 +0900

    chore: member 패키지에 있는 데이터에 주석 추가

--- my-garden-be/src/main/java/org/hyunggi/mygardenbe/member/entity/MemberEntity.java
+++ my-garden-be/src/main/java/org/hyunggi/mygardenbe/member/entity/MemberEntity.java
@@ -13,22 +13,43 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Collection;
 
+/**
+ * 유저 Entity
+ */
 @Getter
 @NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 @Entity
 public class MemberEntity extends BaseEntity implements UserDetails {
+    /**
+     * 유저 ID
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    /**
+     * 유저 이메일
+     */
     @Column(length = 50, nullable = false, unique = true)
     private String email;
+
+    /**
+     * 유저 비밀번호
+     */
     @Column(length = 100, nullable = false)
     private String password;
+
+    /**
+     * 유저 권한
+     */
     @Getter
     @Enumerated(EnumType.STRING)
     @Column(length = 10, nullable = false)
     private Role role;
 
+    /**
+     * 유저 활성화 여부
+     */
     private boolean enabled;
 
     @Builder(access = lombok.AccessLevel.PRIVATE)
@@ -39,6 +60,13 @@ public class MemberEntity extends BaseEntity implements UserDetails {
         this.enabled = enabled;
     }
 
+    /**
+     * 유저 Entity 생성
+     *
+     * @param member          유저 Domain
+     * @param passwordEncoder 비밀번호 암호화에 사용할 PasswordEncoder
+     * @return 유저 Entity
+     */
     public static MemberEntity of(final Member member, final PasswordEncoder passwordEncoder) {
         validate(member, passwordEncoder);
 
@@ -50,6 +78,12 @@ public class MemberEntity extends BaseEntity implements UserDetails {
                 .build();
     }
 
+    /**
+     * 유저 Entity 생성 인자 유효성 검증
+     *
+     * @param member          유저 Domain
+     * @param passwordEncoder 비밀번호 암호화에 사용할 PasswordEncoder
+     */
     private static void validate(final Member member, final PasswordEncoder passwordEncoder) {
         if (member == null) {
             throw new IllegalArgumentException("Member는 null이 될 수 없습니다.");
@@ -60,36 +94,71 @@ public class MemberEntity extends BaseEntity implements UserDetails {
         }
     }
 
+    /**
+     * 해당 유저 Entity의 권한 목록 반환
+     *
+     * @return 권한 목록
+     */
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return this.role.getAuthorities();
     }
 
+    /**
+     * 해당 유저 Entity의 비밀번호 반환
+     *
+     * @return 비밀번호
+     */
     @Override
     public String getPassword() {
         return this.password;
     }
 
+    /**
+     * 해당 유저 Entity의 이메일 반환
+     *
+     * @return 이메일
+     */
     @Override
     public String getUsername() {
         return this.email;
     }
 
+    /**
+     * 해당 유저 Entity의 계정 만료 여부 반환
+     *
+     * @return 계정 만료 여부
+     */
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
 
+    /**
+     * 해당 유저 Entity의 계정 잠금 여부 반환
+     *
+     * @return 계정 잠금 여부
+     */
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
 
+    /**
+     * 해당 유저 Entity의 비밀번호 만료 여부 반환
+     *
+     * @return 비밀번호 만료 여부
+     */
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
 
+    /**
+     * 해당 유저 Entity의 활성화 여부 반환
+     *
+     * @return 활성화 여부
+     */
     @Override
     public boolean isEnabled() {
         return this.enabled;
--- my-garden-be/src/main/java/org/hyunggi/mygardenbe/member/repository/MemberRepository.java
+++ my-garden-be/src/main/java/org/hyunggi/mygardenbe/member/repository/MemberRepository.java
@@ -5,6 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+/**
+ * 유저 Entity Repository
+ */
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+    /**
+     * 유저 Entity를 이메일로 조회
+     *
+     * @param email 이메일
+     * @return 유저 Entity
+     */
     Optional<MemberEntity> findByEmail(final String email);
 }
